1bcc196112a1953e22756036c7d46464095e9525
v011660 <michael.engel@vkb.de>
2025-04-30 13:47:42 +0200

    component hinzugefügt

---
+++ frontend/src/components/DummyItem.vue
@@ -0,0 +1,14 @@
+<template>
+    <q-item>
+      <q-item-section>
+        Name: {{ name }}, Alter: {{ age }}
+      </q-item-section>
+    </q-item>
+  </template>
+  
+  <script setup>
+  defineProps({
+    name: String,
+    age: Number
+  });
+  </script>
--- frontend/src/pages/GetPage.vue
+++ frontend/src/pages/GetPage.vue
@@ -1,49 +1,50 @@
 <template>
-    <div class="btn-wrapper">
-      <q-btn outline rounded color="primary" label="Get" @click="get" />
-    </div>
-    <div class="wrapper">
-      <q-list class="list" bordered separator>
-        <q-item v-for="item in list">
-          <q-item-section>
-            Name: {{ item.name }}, Alter: {{ item.age }}
-          </q-item-section>
-        </q-item>
-      </q-list>
-    </div>
-  </template>
-  
-  <script setup>
-  import { ref } from "vue";
-  
-  const list = ref();
-  
-  const get = () => {
-    fetch("http://localhost:8080/dummies")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        list.value = data;
-      });
-  };
-  </script>
-  
-  <style scoped>
-  .list {
-    width: 50vw;
-  }
-  .wrapper {
-    height: 50vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  
-  .btn-wrapper {
-    display: flex;
-    justify-content: center;
-    margin: 20pt;
-  }
-  </style>
-  
+  <div class="btn-wrapper">
+    <q-btn outline rounded color="primary" label="Get" @click="get" />
+  </div>
+  <div class="wrapper">
+    <q-list class="list" bordered separator>
+      <DummyItem
+        v-for="item in list"
+        :key="item.name + item.age"
+        :name="item.name"
+        :age="item.age"
+      />
+    </q-list>
+  </div>
+</template>
+
+<script setup>
+import DummyItem from "components/DummyItem.vue";
+import { ref } from "vue";
+
+const list = ref();
+
+const get = () => {
+  fetch("http://localhost:8080/dummies")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      list.value = data;
+    });
+};
+</script>
+
+<style scoped>
+.list {
+  width: 50vw;
+}
+.wrapper {
+  height: 50vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.btn-wrapper {
+  display: flex;
+  justify-content: center;
+  margin: 20pt;
+}
+</style>
